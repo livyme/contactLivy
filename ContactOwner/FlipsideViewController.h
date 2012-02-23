00cbe76320a4e1fa1f50f468d4ecc24351b2d562
Livy <Livy@McLivy.local>
2012-02-22 18:41:32 -0600

    Initial Commit

---
+++ ContactOwner/FlipsideViewController.h
@@ -0,0 +1,23 @@
+//
+//  FlipsideViewController.h
+//  ContactOwner
+//
+//  Created by 李 威 on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FlipsideViewController;
+
+@protocol FlipsideViewControllerDelegate
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller;
+@end
+
+@interface FlipsideViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet id <FlipsideViewControllerDelegate> delegate;
+
+- (IBAction)done:(id)sender;
+
+@end
---
+++ ContactOwner/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  ContactOwner
+//
+//  Created by 李 威 on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate, UIPopoverControllerDelegate>
+
+@property (strong, nonatomic) UIPopoverController *flipsidePopoverController;
+
+@end
